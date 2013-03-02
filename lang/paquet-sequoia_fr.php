cc0b6c147dc4c6ca4f943d2ee46480353a71b1d4
kent1 <kent1@arscenic.info>
2013-03-02 17:39:38 +0100

    Les locutions pour le paquet.xml

---
+++ lang/paquet-sequoia_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/zeroclipboard/trunk/lang
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'sequoia_description' => 'Surcharge du squelette Géodiversité pour le projet Sequoia Act (http://www.sequoia-act.net)',
+	'sequoia_nom' => 'Géodiversité - Sequoia',
+	'sequoia_slogan' => 'Surcharge du squelette Géodiversité pour le projet Sequoia Act'
+);
+
+?>
