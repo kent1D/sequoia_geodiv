279f0d6c5e5b942bdd353f1d59c287b9c9603132
kent1 <kent1@arscenic.info>
2013-03-29 17:44:39 +0100

    Editer le titre des rubriques depuis l'espace public
    Le fichier de langue espagnol

---
+++ lang/sequoia_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_voir_carte' => 'Ver el mapa',
+	
+	// T
+	'titre_carte' => 'el mapa',
+	'titre_contact' => 'Contacto'
+	
+);
+
+?>
