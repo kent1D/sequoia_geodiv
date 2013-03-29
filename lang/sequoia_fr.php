ba79f65bfef688f80ca006c113e55c98bb4055a9
kent1 <kent1@arscenic.info>
2013-03-29 10:03:03 +0100

    Un fichier de langue qui sert pour l'instant principalement sur la home

---
+++ lang/sequoia_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_voir_carte' => 'Voir la carte',
+	
+	// T
+	'titre_carte' => 'la carte',
+	'titre_contact' => 'Contact'
+	
+);
+
+?>
